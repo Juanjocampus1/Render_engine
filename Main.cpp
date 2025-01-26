eccc82c5c67d9451bcf4fd8203243e066228f769
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-01-26 14:38:11 +0100

    reorganizamos el codigo, añadimos la posibilidad de tener indices con el ebo y separamos los shaders a archivos .glsl

--- Main.cpp
+++ Main.cpp
@@ -2,35 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main(){\n"
-"{\n"
-"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main(){\n"
-"FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\0";
+#include "Header_Files/ShaderClass.h"
+#include "Header_Files/VAO.h"
+#include "Header_Files/VBO.h"
+#include "Header_Files/EBO.h"
 
 int SCR_WIDTH = 800;
 int SCR_HEIGHT = 800;
 
-int main(){
-    
+GLfloat vertices[] = {
+	-0.5f,    -0.5f * float(sqrt(3)) / 3, 0.0f,
+	 0.5f,    -0.5f * float(sqrt(3)) / 3, 0.0f,
+	 0.0f,     0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+	 0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+	 0.0f,    -0.5f * float(sqrt(3)) / 3, 0.0f
+};
+
+GLuint indices[] = {
+	0, 3, 5,
+	3, 2, 4,
+	5, 4, 1
+};
+
+int main(void) {
+
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
 
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "lluanllax-Engine", NULL, NULL);
 
@@ -41,53 +42,42 @@ int main(){
 	}
 
 	glfwMakeContextCurrent(window);
-
 	gladLoadGL();
-
 	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+	Shader shaderProgram("Shaders/VertexShader.glsl", "Shaders/FragmentShader.glsl");
 
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+	VAO VAO1;
+	VAO1.Bind();
 
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
 
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	VAO1.LinkVBO(VBO1, 0);
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+	
 
-	GLuint VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-
-	while (!glfwWindowShouldClose(window)){
+	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		shaderProgram.Activate();
+		VAO1.Bind();
+
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 	}
 
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
 	glfwDestroyWindow(window);
 	glfwTerminate();
-    return 0;
+	return 0;
 }
